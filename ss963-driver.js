5464034b5428d9cf807aa8ecc3bbb83183133f49
Özgür Koca <ozgurkoca@gmail.com>
2020-06-12 05:12:29 +0300

    config.node iptal edildi. servis dosyasini admin API ile okuyor.

--- ss963-driver.js
+++ ss963-driver.js
@@ -5,76 +5,27 @@ module.exports = function(RED) {
 
 	const NODE_NAME = 'ss963 Service Node';
 
-    function ss963ConfigNode(n) {
-        RED.nodes.createNode(this, n);
-        this.configLabel = n.configLabel;
-        this.portCount = n.portCount;
-        this.latchPin = n.latchPin;
-        this.latchDelay = n.latchDelay;
-        this.speed = n.speed;
-        this.loopDelayUs = n.loopDelayUs;
-    }
-    RED.nodes.registerType('ss963-config', ss963ConfigNode);
-
 	/*
 		---------------------------------------------------------------
 	*/
     function driverNode(config) {
         RED.nodes.createNode(this, config);
         var node = this;
-		node.configNode = RED.nodes.getNode(config.configName);
 
-		// Check Raspberry Pi Hardware
-		var isPi = require('detect-rpi');
-		if (!isPi())
-			node.error("This is Raspberry Pi specific node.");
+		is_raspberry();
+		is_spi_exists();
+		is_service_running();;
 
-
-		// Check SPI Enabled
-		const fs = require('fs')
-		if (!fs.existsSync('/dev/spidev0.0'))
-			node.error("There is no SPI port!");
-
-		// Check SHM Listener Service
-		const { exec } = require("child_process");
-		exec("sudo systemctl status apache2 | grep active",
-			(error, stdout, stderr) => {
-				if (!stdout.includes("Active: active (running)")) {
-					node.error("SHM Listener driver not loaded.");
-					node.status( {fill:"red", shape:"dot", text: "Servis is not runnig!"} );
-				} else
-					node.status( {fill:"green", shape:"dot", text: "ss963.service is runnig!"} );
-			});
-
-		// Reply ss963.service.conf file as JSON
-		RED.httpAdmin.get("/getServiceConfig", RED.auth.needsPermission('ss963Service.read'), function(req,res) {
-		    const fs = require('fs');
-		    const ini = require('ini');
-			const path = require("path");
-		    const config = ini.parse(fs.readFileSync(path.resolve(__dirname, "service.conf"), 'utf-8'));
-		    const degreeType = config.PORT_COUNT;
-		    console.log(config);
-			console.log(degreeType);
-		});
-
-
-//		node.portCount = configNode.portCount;
-//    	node.latchPin = configNode.latchPin;
-//    	node.latchDelay = configNode.latchDelay;
-//		node.status( {fill:"red", shape:"dot", text: node.portCount+"-"+node.latchPin+"-"+node.latchDelay} );
-
-//      var configNode = RED.nodes.node(config.configLabel);
 
 		node.on('close', function() {
         	RED.log.info(NODE_NAME + ': Node closed.');
 			RED.comms.publish('A message to admin');
-	   });
+		});
 
-        node.on('input', function(msg) {
-
+		node.on('input', function(msg) {
 
 			try {
-		        node.status({ fill:"green", shape:"dot", text: node.configNode.portCount});
+		        node.status({ fill:"green", shape:"dot", text: node.portCount});
 				// Array.isArray(msg.payload))
 
 			} catch (error) {
@@ -87,6 +38,47 @@ module.exports = function(RED) {
 	            node.send(msg);
 			}
         });
+
+
+		// ----------------------------------------------------------
+
+		function  is_raspberry() {
+	        // Check Raspberry Pi Hardware
+	        var isPi = require('detect-rpi');
+	        if (!isPi())
+	            node.error("This is Raspberry Pi specific node.");
+		}
+
+		function is_spi_exists() {
+	        // Check SPI Enabled
+	        const fs = require('fs')
+    	    if (!fs.existsSync('/dev/spidev0.0'))
+	            node.error("There is no SPI port!");
+		}
+
+		function is_service_running() {
+	        // Check SHM Listener Service
+	        const { exec } = require("child_process");
+	        exec("sudo systemctl status apache2 | grep active",
+	            (error, stdout, stderr) => {
+	                if (!stdout.includes("Active: active (running)")) {
+	                    node.error("SHM Listener driver not loaded.");
+	                    node.status( {fill:"red", shape:"dot", text: "Servis is not runnig!"} );
+	                } else
+	                    node.status( {fill:"green", shape:"dot", text: "ss963.service is runnig!"} );
+    	        });
+		}
+
+
+		// --------------------------------------
+        RED.httpAdmin.get("/getServiceConfFile", RED.auth.needsPermission('ss963Service.read'), function(req,res) {
+            const fs = require('fs');
+            const ini = require('ini');
+            const path = require("path");
+            const config = ini.parse(fs.readFileSync(path.resolve(__dirname, "service.conf"), 'utf-8'));
+            res.json(config);
+        });
+
     }
     RED.nodes.registerType("ss963-driver", driverNode);
 }
