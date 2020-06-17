1eda4630f2964c118e64cb8b817bcaf8fded4d0e
Özgür Koca <ozgurkoca@gmail.com>
2020-06-17 23:59:57 +0300

    Buggy: Syntax error. MOSI write ok.

--- ss963-driver.js
+++ ss963-driver.js
@@ -14,7 +14,7 @@ module.exports = function(RED) {
 
 		is_raspberry();
 		is_spi_exists();
-		is_service_running();;
+		is_service_running();
 
 		node.portCount = config.portCount;
 		node.latchPin = config.latchPin;
@@ -22,12 +22,8 @@ module.exports = function(RED) {
 		node.speed = config.speed;
 		node.loopDelayUs = config.loopDelayUs;
 
-node.error("testtttt-111111");
-
 		// -------------------------------------------
 		node.on('input', function(msg) {
-
-node.error("testtttt");
 
             const path = require("path");
             const shm = require(path.resolve(__dirname, "node_modules/shm-typed-array/index.js"));
@@ -35,31 +31,29 @@ node.error("testtttt");
 
 			try {
 
-node.log(node.portCount);
-node.log(Math.ceil(node.portCount/8));
-
 				var SHM_size = Math.ceil(node.portCount/8);
 				SHM_arr = shm.create(SHM_size, 'Uint8Array', SHM_SEGMET_KEY);
 
-				node.log(msg.payload[1]);
+				// Convert binary presented payload array to new decimal array
+				var i = 0;
+				for (var dec = 0; dec < SHM_size; dec++) {
+					var octal = 0;
+					for (var bits = 0; bits < 8; bits++) {
+						octal += msg.payload[i] *  Math.pow(2, bits);
+						i++;
+					}
+					SHM_arr[dec] = octal;
+				}
 
-				SHM_arr[0] = Math.floor(Math.random() * 255);
-				SHM_arr[1] = Math.floor(Math.random() * 255);
-
-
-
-				SHM_arr = shm.get(SHM_SEGMET_KEY, 'Uint8Array');
-				node.log(SHM_arr);
-
+				//SHM_arr = shm.get(SHM_SEGMET_KEY, 'Uint8Array');
 
 			} catch (error) {
-                node.status( {fill:"red", shape:"dot", text:error.message} );
+                node.status( {fill:"red", shape:"dot", text: error.message} );
                 node.error(error.message);
                 throw(error);
 
 			} finally {
 	            node.send(msg);
-				node.done();
 				shm.detach(SHM_SEGMET_KEY);
 			}
         });
@@ -90,11 +84,14 @@ node.log(Math.ceil(node.portCount/8));
 	        const { exec } = require("child_process");
 	        exec("sudo systemctl status spi.service | grep active",
 	            (error, stdout, stderr) => {
+
 	                if (!stdout.includes("Active: active (running)")) {
 	                    node.error("SHM Listener driver not loaded.");
 	                    node.status( {fill:"red", shape:"dot", text: "Service is not runnig!"} );
-	                } else
-	                    node.status( {fill:"green", shape:"dot", text: "Service is runnig"} );
+	                } else {
+	                    node.status( {fill:"green", shape:"dot", text: "Service is running"} );
+					}
+
     	        });
 		}
 
@@ -112,18 +109,17 @@ node.log(Math.ceil(node.portCount/8));
                 SPEED: req.query.SPEED,
                 LOOP_DELAY_US: req.query.LOOP_DELAY_US
 			}
-			fs.writeFileSync(path.resolve(__dirname, "spi.service.conf"), ini.stringify(config, {}))
+			fs.writeFileSync(path.resolve(__dirname, "spi.service.conf"), ini.stringify(config, {}));
 			const { exec } = require("child_process");
 			exec("sudo mv " + path.resolve(__dirname, "spi.service.conf") + " /etc/spi.service.conf", (error, stdout, stderr) => {
 				node.log(stderr);
 			});
 			exec("sudo systemctl restart spi.service");
-			node.status( {fill:"blue", shape:"ring", text: "Service restarted."} );
+			node.status( {fill:"green", shape:"ring", text: "Service restarted."} );
         });
 
 		// -------------------------------------------------
         RED.httpAdmin.get("/getServiceConfFile", RED.auth.needsPermission('SPIService.read'), function(req, res) {
-
             const fs = require('fs');
             const ini = require('ini');
             const path = require("path");
